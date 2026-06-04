81fe655c8d7f7cd13057c0242d66ec5f7a15d4d0
Soorya KT <sooryathamilselvan@gmail.com>
2026-06-04 15:10:45 +0530

    Second largest in an array

---
+++ 04-06-26/Second_Largest.py
@@ -0,0 +1,11 @@
+arr=[1,9,3,4,5,4,6,4]
+largest=arr[0]
+second_largest=arr[0]
+for i in arr:
+    if i>largest:
+        second_largest=largest
+        largest=i
+    elif i>second_largest and i <largest:
+        second_largest=i
+
+print(second_largest)
