1c4caa0cb8da8541c9a0d10a17c76495df9a5dd9
Soorya KT <sooryathamilselvan@gmail.com>
2026-07-01 20:34:31 +0530

    Selection Sort

---
+++ 01-07-26/Selection_Sort.py
@@ -0,0 +1,12 @@
+def selectionsort(num):
+    n = len(num)
+    for i in range(0,n):
+        min_index=i
+        for j in range(i+1,n):
+            if num[min_index]>num[j]:
+                j = min_index
+        num[i],num[min_index]=num[min_index],num[i]
+    return num
+text = [1, 2, 5, 4, 7, 5]
+print(selectionsort(text))
+
