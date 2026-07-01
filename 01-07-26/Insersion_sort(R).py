fdfe3cffe659bcb5bc7ee5d0bd19fc0299dcc434
Soorya KT <sooryathamilselvan@gmail.com>
2026-07-01 23:48:58 +0530

    Insertion Sort Reversed

---
+++ 01-07-26/Insersion_sort(R).py
@@ -0,0 +1,10 @@
+nums = [3, 5, 1, 7, 9, 2, 4]
+n = len(nums)
+for i in range(1, n):
+    key = nums[i]
+    j = i - 1
+    while j >= 0 and nums[j] < key:
+        nums[j + 1] = nums[j]
+        j -= 1
+    nums[j + 1] = key
+print(nums)
